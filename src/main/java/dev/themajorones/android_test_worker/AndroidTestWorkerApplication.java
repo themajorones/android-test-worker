ce8a02890972700a81a8e7c862fd12c59afd4278
iamSlightlyWind <iam.slightlywind@themajorones.dev>
2026-05-13 23:32:29 +0700

    tree(prj): init

---
+++ src/main/java/dev/themajorones/android_test_worker/AndroidTestWorkerApplication.java
@@ -0,0 +1,13 @@
+package dev.themajorones.android_test_worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndroidTestWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndroidTestWorkerApplication.class, args);
+	}
+
+}
